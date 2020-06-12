0094b6458ef462964eda93e9148210d050f654f9
leandrorangel94 <rangel94@outlook.com>
2020-06-11 23:11:43 -0300

    funcionalidades avançadas: validação

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -50,21 +50,22 @@ class PointsController {
     return response.json(serializedPoints);
   }
 
-  async create(request: Request, response: Response){
+  async create(request: Request, response: Response) {
     const {
-      name, 
+      name,
       email,
       whatsapp,
-      latitude, 
+      latitude,
       longitude,
       city,
-      uf, 
+      uf,
       items
     } = request.body;
   
     const trx = await knex.transaction();
+  
     const point = {
-      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename, 
       name,
       email,
       whatsapp,
@@ -73,17 +74,20 @@ class PointsController {
       city,
       uf
     };
-  
+
     const insertedIds = await trx('points').insert(point);
   
     const point_id = insertedIds[0];
   
-    const pointItems = items.map((item_id: number) =>{
-      return {
-        item_id,
-        point_id,
-      };
-    })
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id
+        };
+      });
   
     await trx('point_items').insert(pointItems);
   
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import { Map, TileLayer, Marker} from 'react-leaflet';
 import {LeafletMouseEvent} from 'leaflet';
 
+import Dropzone from '../../components/Dropzone';
+
 import api from '../../services/api';
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -42,6 +44,7 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -121,22 +124,29 @@ const CreatePoint = () => {
   async function handleSubmit(event: FormEvent){
     event.preventDefault();
 
+
+
     const {name, email, whatsapp} = formData;
     const uf = selectedUf;
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+    const data = new FormData();
+
+    
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+    
+      if(selectedFile){
+        data.append('image', selectedFile);
+      }
 
     await api.post('points', data);
 
@@ -158,6 +168,8 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/>ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
